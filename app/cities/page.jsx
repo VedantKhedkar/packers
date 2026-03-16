bb54a660e64fede4918782680bdaa40c7edd03d2
vedantkhedkar <vedantkhedkar4@gmail.com>
2026-03-16 13:47:52 +0530

    xg

--- app/cities/page.jsx
+++ app/cities/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 import Link from 'next/link';
 
 export default function CitiesPage() {
